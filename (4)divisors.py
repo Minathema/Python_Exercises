7a55e0bd342a69ebdc4c54a33c27152ebba57adc
Minathema <36901826+Minathema@users.noreply.github.com>
2019-02-19 18:25:39 +0200

    Delete (4)divisors.py

--- (4)divisors.py
+++
@@ -1,9 +0,0 @@
-def exercise4():
-    list = []
-    num = input('Please pick a number: ')
-    for x in range(1, int(num) + 1):
-        if int(num) % x == 0 :
-            list.append(x)
-    print (list)
-
-exercise4()
